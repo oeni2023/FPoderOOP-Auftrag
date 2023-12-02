46dd8f8f2cf0ac710101756acd460c10ea3112e8
oeni2023 <141681986+oeni2023@users.noreply.github.com>
2023-12-02 20:07:53 +0100

    scripts.js : buttons defined, eventListener

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+
+
+/* Define  HTML elements */
+/* Buttons for Click-events */
+const btnAdd = document.getElementById('add-button');
+const btnRand = document.getElementById('rand-button');
+
+
+/* Define eventListener */
+btnAdd.addEventListener('click', () => {
+    console.log("'Add' clicked!");
+});
+
