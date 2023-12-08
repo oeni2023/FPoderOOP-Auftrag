caae2b13ba61c50ced378f6f7fb348a37d08206b
oeni2023 <141681986+oeni2023@users.noreply.github.com>
2023-12-08 09:59:51 +0100

    restarted

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,21 +4,21 @@
 /* Buttons for Click-events */
 const btnAdd = document.getElementById('add-button');
 const btnRand = document.getElementById('rand-button');
-const statsDiv = document.getElementById('status-wrapper');
+const statusDiv = document.getElementById('status-wrapper');
 const inputName = document.getElementById('input-name');
 const inputColor = document.getElementById('input-color');
 const inputNambers = document.getElementById('input-numbers');
+const resultDiv = document.getElementById('result-wrapper')
 
 
 /* Define eventListener */
 btnAdd.addEventListener('click', () => {
     console.log("'Add' clicked!");
 
+    //checkName();
     removeMessage();
     addMessage();
-
-
-
+    //createTiles()
 });
 
 btnRand.addEventListener('click', () => {
@@ -26,27 +26,70 @@ btnRand.addEventListener('click', () => {
 });
 
 
+/* Define Functions */
+/* Check name field */
+function checkName() {
+    if ( inputName.value.length < 3 || inputName.value.length > 10) {
+        console.log("name no correct");
 
-function addMessage() {
+    }
+    return true;
+}
+
+/* Check color field */
+function checkColor() {
+
+}
+
+/* Check numbers field */
+function ckeckNumbers() {
+
+}
+
+/* Print Message to Screen */
+function printMessage() {
+    if ( checkName() ) {
+        console.log('Bingo!!!');
+    } else {
+        console.log('No good!!');
+    }
+
+
+}
+/* Create tiles */
+function createTiles() {
     const elementType = 'div';
     const elementText = inputName.value;
     const elementClass = 'ok';
-    // const elementAttribute = 'id';
-    // const elementId = 'status-text';
-    // const elementBorderStyle = '3px solid red';
-    const elementParent = statsDiv;
+    const elementParent = resultDiv;
+
+    const tileGenerator = new HtmlGenerator(elementType, elementText);
+    tileGenerator.addClass(elementClass);
+    tileGenerator.appendTo(elementParent);
+
+}
+
+
+/* Create status message */
+function addMessage() {
+    const elementType = 'div';
+    const elementText = inputName.value;
+    const elementClass = 'error';
+    const elementParent = statusDiv;
 
     const messageGenerator = new HtmlGenerator(elementType,  elementText);
     messageGenerator.addClass(elementClass);
-    //messageGenerator.setAttribute(elementAttribute, elementId);
+    messageGenerator.addClass('element-border');
     messageGenerator.appendTo(elementParent);
 
     inputName.value = ''
 }
 
 
+
+
 function removeMessage() {
-    statsDiv.innerHTML= '';
+    statusDiv.innerHTML= '';
 }
 
 class HtmlGenerator {
