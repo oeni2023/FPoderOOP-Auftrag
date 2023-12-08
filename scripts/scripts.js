a1b7d6a0f1bbb3e3b429b0089ff938dcc1b0e6cf
oeni2023 <141681986+oeni2023@users.noreply.github.com>
2023-12-08 11:11:48 +0100

    Check functions finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,55 +7,82 @@ const btnRand = document.getElementById('rand-button');
 const statusDiv = document.getElementById('status-wrapper');
 const inputName = document.getElementById('input-name');
 const inputColor = document.getElementById('input-color');
-const inputNambers = document.getElementById('input-numbers');
-const resultDiv = document.getElementById('result-wrapper')
+const inputNumbers = document.getElementById('input-numbers');
+const resultDiv = document.getElementById('result-wrapper');
 
 
 /* Define eventListener */
+/* Add - Button*/
 btnAdd.addEventListener('click', () => {
     console.log("'Add' clicked!");
 
-    //checkName();
     removeMessage();
-    addMessage();
+
+    if (!(checkName() && checkColor() && ckeckNumbers())) {
+        console.log('Check not successful!');
+        const messageSuccess = 'Eingabe NICHT korrekt! - Bitte versuchen sie es nochmal.';
+        const messageClass = 'error';
+        addMessage(messageSuccess, messageClass);
+    } else {
+        console.log('Check successful!');
+        const messageSuccess = 'Eingabe korrekt!';
+        const messageClass = 'ok';
+        addMessage(messageSuccess, messageClass);
+    }
+
     //createTiles()
 });
 
+/* Rand - Button */
 btnRand.addEventListener('click', () => {
    console.log("'Rand' clicked!")
 });
-
 
 /* Define Functions */
 /* Check name field */
 function checkName() {
     if ( inputName.value.length < 3 || inputName.value.length > 10) {
-        console.log("name no correct");
-
+        console.log("name not correct");
+        inputName.classList.add('error');
+        return false;
     }
+    inputName.classList.add('ok');
+    inputName.disabled = true;
     return true;
 }
 
 /* Check color field */
 function checkColor() {
+    const colorValue = inputColor.value;
+    const colorPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 
+    if (!(colorValue.length > 0 && colorPattern.test(colorValue))) {
+        console.log("color not correct");
+        inputColor.classList.add('error');
+        return false;
+    }
+    inputColor.classList.add('ok');
+    inputColor.disabled = true;
+    return true;
 }
 
 /* Check numbers field */
 function ckeckNumbers() {
+    const arrayNumbers = inputNumbers.value.split(',');
+    const numberPattern = /^[0-9]+(,[0-9]+)*$/;
+    console.log(arrayNumbers);
 
-}
-
-/* Print Message to Screen */
-function printMessage() {
-    if ( checkName() ) {
-        console.log('Bingo!!!');
-    } else {
-        console.log('No good!!');
+    if ( !(arrayNumbers.length > 0 && numberPattern.test(arrayNumbers))) {
+        console.log("numbers not correct");
+        inputNumbers.classList.add('error');
+        return false;
     }
-
-
+    inputNumbers.classList.add('ok');
+    inputNumbers.disabled = true;
+    return true;
 }
+
+
 /* Create tiles */
 function createTiles() {
     const elementType = 'div';
@@ -71,21 +98,16 @@ function createTiles() {
 
 
 /* Create status message */
-function addMessage() {
+function addMessage(messageText, messageClass) {
     const elementType = 'div';
-    const elementText = inputName.value;
-    const elementClass = 'error';
+    const elementText = messageText;
+    const elementClass = messageClass;
     const elementParent = statusDiv;
 
     const messageGenerator = new HtmlGenerator(elementType,  elementText);
     messageGenerator.addClass(elementClass);
-    messageGenerator.addClass('element-border');
     messageGenerator.appendTo(elementParent);
-
-    inputName.value = ''
 }
-
-
 
 
 function removeMessage() {
